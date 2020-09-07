ee8b0ad569295bee5a2e4a4e4aecabef6d5cda77
TomasVotruba <tomas.vot@gmail.com>
2020-09-07 03:34:18 +0200

    typo

---
+++ config/config.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Migrify\StaticDetector\NodeTraverser\StaticCollectNodeTraverser;
+use Migrify\StaticDetector\NodeTraverser\StaticCollectNodeTraverserFactory;
+use PhpParser\Parser;
+use PhpParser\ParserFactory;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Finder\Finder;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire();
+
+    $services->load('Migrify\StaticDetector\\', __DIR__ . '/../src')
+        ->exclude([
+            __DIR__ . '/../src/Exception',
+            __DIR__ . '/../src/ValueObject',
+            __DIR__ . '/../src/HttpKernel/StaticDetectorKernel.php',
+        ]);
+
+    $services->set(StaticCollectNodeTraverser::class)
+        ->factory([ref(StaticCollectNodeTraverserFactory::class), 'create']);
+
+    $services->set(Finder::class);
+
+    $services->set(ParserFactory::class);
+    $services->set(Parser::class)
+        ->factory([ref(ParserFactory::class), 'create'])
+        ->arg('$kind', ParserFactory::PREFER_PHP7);
+};
